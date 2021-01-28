27b511afc63ac7f76ec6c0f7c97e0f40c02b71ec
Danilo Lekovic <danilo@lekovic.ca>
2021-01-27 18:26:20 -0800

    jquery, toggle between experience

---
+++ js/danilo.js
@@ -0,0 +1,20 @@
+$("#canalyst-show").click(function() {
+    $(".exerience-label-active").removeClass("exerience-label-active");
+    $("#canalyst-show").addClass("exerience-label-active");
+    $(".experience-active").removeClass("experience-active");
+    $("#canalyst").addClass("experience-active");
+});
+
+$("#ips-show").click(function() {
+    $(".exerience-label-active").removeClass("exerience-label-active");
+    $("#ips-show").addClass("exerience-label-active");
+    $(".experience-active").removeClass("experience-active");
+    $("#ips").addClass("experience-active");
+});
+
+$("#bb-show").click(function() {
+    $(".exerience-label-active").removeClass("exerience-label-active");
+    $("#bb-show").addClass("exerience-label-active");
+    $(".experience-active").removeClass("experience-active");
+    $("#best-buy").addClass("experience-active");
+});
