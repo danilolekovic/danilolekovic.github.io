2502bfbf4a613a7d8332a9f2b2c5121dcb5c6191
Danilo Lekovic <danilo@lekovic.ca>
2021-10-11 13:35:06 -0700

    Music

--- music/js/music.js
+++ music/js/music.js
@@ -1,14 +1,15 @@
 $.getJSON("https://raw.githubusercontent.com/danilolekovic/danilolekovic.github.io/main/music/music.json", function(json) {
-    const songs = json["songs"];
+    const songs = json["items"];
 
     for (var song in songs) {
         var currentSong = songs[song];
+        var name = "" + currentSong["track"]["name"];
 
-        if (currentSong["Track name"].length > 26) {
-            currentSong["Track name"] = currentSong["Track name"].substr(0, 26) + "..";
+        if (currentSong["track"]["name"].length > 26) {
+            name = currentSong["track"]["name"].substr(0, 26) + "..";
         }
 
-        $("#songs").append("<li><img class='album-art' src='" + currentSong["Art"] + "'/><span class='track'>" 
-        + currentSong["Track name"] + "</span><span class='artist'>" + currentSong["Artist name"] + "</span></li>");
+        $("#songs").append("<li><img class='album-art' src='" + currentSong["track"]["album"]["images"][0]["url"] + "'/><span class='track'>" 
+        + name + "</span><span class='artist'>" + currentSong["track"]["album"]["artists"][0]["name"] + "</span></li>");
     }
 });
