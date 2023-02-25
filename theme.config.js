e766e121716c85f95213da3eb327ccdb1beca5c9
Danilo Lekovic <danilo_lekovic@sfu.ca>
2023-02-25 12:01:13 -0800

    Website stuff

---
+++ theme.config.js
@@ -0,0 +1,29 @@
+const YEAR = new Date().getFullYear()
+
+export default {
+  footer: (
+    <footer>
+      <small>
+        <time>{YEAR}</time> © Danilo Lekovic
+        <a href="/feed.xml">RSS</a>
+      </small>
+      <style jsx>{`
+        footer {
+          margin-top: 8rem;
+        }
+        a {
+          float: right;
+        }
+      `}</style>
+    </footer>
+  ),
+  head: ({ title, meta }) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
+}
