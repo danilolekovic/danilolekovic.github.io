c983368cd7aa5235b292a92cf9b51d8e78ba4236
Danilo Lekovic <danilo_lekovic@sfu.ca>
2023-03-01 19:45:45 -0800

    Fixing setup

--- next.config.js
+++ next.config.js
@@ -9,4 +9,12 @@ const nextConfig = {
   // any configs you need
 }
 
-module.exports = withNextra(nextConfig)
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
+  ...withNextra(nextConfig),
+}
